af28b57231f8972abc9a98f651180380e04e3bd0
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-03-27 11:50:38 -0500

    Permissible should be inside Guachiman module

--- lib/guachiman/rails/permissible.rb
+++ lib/guachiman/rails/permissible.rb
@@ -1,29 +1,31 @@
-module Permissible
-  extend ActiveSupport::Concern
+module Guachiman
+  module Permissible
+    extend ActiveSupport::Concern
 
-  included do
-    before_filter :authorize
-  end
+    included do
+      before_filter :authorize
+    end
 
-  def current_permission
-    @current_permission ||= Permission.new current_user
-  end
+    def current_permission
+      @current_permission ||= Permission.new current_user
+    end
 
-  def current_resource
-    nil
-  end
+    def current_resource
+      nil
+    end
 
-  def current_user
-    raise 'This method must be implemented'
-  end
+    def current_user
+      raise 'This method must be implemented'
+    end
 
-  def not_authorized
-    raise 'This method must be implemented'
-  end
+    def not_authorized
+      raise 'This method must be implemented'
+    end
 
-  def authorize
-    if !current_permission.allow?(params[:controller], params[:action], current_resource)
-      not_authorized
+    def authorize
+      if !current_permission.allow?(params[:controller], params[:action], current_resource)
+        not_authorized
+      end
     end
   end
 end
