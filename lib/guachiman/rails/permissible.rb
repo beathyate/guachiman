ec16217ef58d861a6324b1778a54a82c5a1444ce
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-04-04 17:12:39 -0500

    fix syntax

--- lib/guachiman/rails/permissible.rb
+++ lib/guachiman/rails/permissible.rb
@@ -28,10 +28,9 @@ module Guachiman
 
     def not_authorized
       if current_user
-          redirect_to :root, alert: t(:not_authorized)
-        else
-          redirect_to :login, alert: t(:please_login)
-        end
+        redirect_to :root, alert: t(:not_authorized)
+      else
+        redirect_to :login, alert: t(:please_login)
       end
     end
   end
