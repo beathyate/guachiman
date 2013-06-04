d7893a41416948541ee7677aae4b53439a6b0d07
Gustavo Beathyate <gustavo@epiclabs.pe>
2013-06-04 12:32:24 -0500

    add strong_parameters require

--- lib/guachiman.rb
+++ lib/guachiman.rb
@@ -1,10 +1,11 @@
 require 'guachiman/version'
 require 'guachiman/permissions'
 require 'guachiman/params'
+require 'strong_parameters'
 
 if defined? Rails
   require 'guachiman/rails/railtie'
   require 'guachiman/rails/permissible'
 
-  ActiveRecord::Base.send :include, ActiveModel::ForbiddenAttributesProtection if defined? ActiveRecord
+  ActiveRecord::Base.send :include, ActiveModel::ForbiddenAttributesProtection
 end
