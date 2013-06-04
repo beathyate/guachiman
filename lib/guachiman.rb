252635f356946f6216583d350530487a768a4ed9
Gustavo Beathyate <gustavo@epiclabs.pe>
2013-06-04 15:54:59 -0500

    require AR and SP only if Rails is defined

--- lib/guachiman.rb
+++ lib/guachiman.rb
@@ -1,12 +1,12 @@
 require 'guachiman/version'
 require 'guachiman/permissions'
 require 'guachiman/params'
-require 'active_record'
-require 'strong_parameters'
 
 if defined? Rails
   require 'guachiman/rails/railtie'
   require 'guachiman/rails/permissible'
+  require 'strong_parameters'
+  require 'active_record'
 
   ActiveRecord::Base.send :include, ActiveModel::ForbiddenAttributesProtection
 end
